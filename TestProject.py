6d58bf71e655b2cfed68178d074e17df34dc3eaa
Mahdi Aghaei <mahdiaghaei.official@gmail.com>
2025-04-14 12:56:23 +0330

    new

--- TestProject.py
+++ TestProject.py
@@ -3,7 +3,7 @@ sys.path.append('.')
 from llmastar.pather import AStar, LLMAStar
 
 query = {
-    "start": [5, 5], "goal": [27, 15], "size": [51, 31],
+    "start": [10, 5], "goal": [10, 15], "size": [51, 31],
     "horizontal_barriers": [[10, 0, 25], [15, 30, 50]],
     "vertical_barriers": [[25, 10, 22]],
     "range_x": [0, 51], "range_y": [0, 31]
